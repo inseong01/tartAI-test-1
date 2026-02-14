aa54d586109ec8ac18c09797b44f0b2fb9c8f440
inseong01 <insung940@naver.com>
2026-02-14 17:57:21 +0900

    fix: Unmatched route 오류
    - app 디렉터리 위치 src 바깥으로 수정

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,40 @@
-import { Text, View } from "react-native";
+import RenderBottomSheet from '../src/components/bottom-sheet';
+import PageHeader from '../src/features/home/header';
+import InvestCategories from '../src/features/invest/category';
+import InvestRaking from '../src/features/invest/ranking';
+
+import { ProviderGroup } from '../src/provider';
+
+import { ScrollView, StyleSheet } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ProviderGroup>
+      <GestureHandlerRootView style={styles.container}>
+        <PageHeader />
+
+        <ScrollView contentContainerStyle={styles.mainContainer} showsVerticalScrollIndicator={false}>
+          <InvestRaking />
+          <InvestCategories />
+        </ScrollView>
+
+        <RenderBottomSheet />
+      </GestureHandlerRootView>
+    </ProviderGroup>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    paddingTop: 96,
+    backgroundColor: '#F9F9F9',
+  },
+  mainContainer: {
+    gap: 32,
+    paddingTop: 7,
+    paddingBottom: 28,
+  },
+});
